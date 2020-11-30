147526ae9b484f500e82c38c5717a4f478809977
RISNOTL <59351730+RISNOTL@users.noreply.github.com>
2020-11-30 11:51:16 +0800

    Create 113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,12 @@
+public class App implements R{
+    public static void main(String[] args) {
+        System.out.print(boy_filePath+"\n");
+        System.out.print(girl_filePath+"\n");
+        for(int[] a:arr2d){
+            for(int b:a){
+                System.out.print(b+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
