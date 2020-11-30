7f1c6fe4c6b02c1ef04454eaa5ddb9819ddaf8f7
RISNOTL <59351730+RISNOTL@users.noreply.github.com>
2020-11-30 11:42:27 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,11 @@
+public class App extends R{
+    public static void main(String[] args) {
+        System.out.print(boy_filePath+"\n");
+        System.out.print(girl_filePath+"\n");
+        for(int[] a:arr2d){
+            for(int b:a){
+                System.out.print(b+"\n");
+            }
+        }
+    }
+}
